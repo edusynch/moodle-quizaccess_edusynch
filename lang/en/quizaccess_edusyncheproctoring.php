7d8c40c742c11f92d4338e4450ad8b7e1941e3ad
Clenisson Calaça <clenisson.calaca@gmail.com>
2022-05-27 07:31:05 -0300

    FIX translations

--- lang/en/quizaccess_edusyncheproctoring.php
+++ lang/en/quizaccess_edusyncheproctoring.php
@@ -8,14 +8,16 @@ $string['pluginname'] = 'EduSynch E-Proctoring';
 
 // Events
 $string['EVENT_MOVE_FOCUS'] = 'Opened a new window or tab; Minimized the browser; Opened other program;';
-$string['EVENT_CLOSED_WINDOW'] = 'Closed browser or tab; refreshed the pag';
-$string['EVENT_CLOSED_WINDOW'] = 'Closed browser or tab; refreshed the pag';
+$string['EVENT_CLOSED_WINDOW'] = 'Closed browser or tab; refreshed the page';
 $string['EVENT_OFFLINE'] = 'Lost connection';
 $string['EVENT_ONLINE'] = 'Connection established';
 $string['EVENT_ONLINE_SERVER'] = 'Connection established with socket';
 $string['EVENT_UI_EVENT'] = 'User Interface moved';
 $string['EVENT_START_SIMULATION'] = 'Exam started';
 $string['EVENT_FINISH_SIMULATION'] = 'Exam completed successfully';
+$string['EVENT_NEW_TAB_WINDOW'] = 'Opened a new window or tab';
+
+
 
 // Capabilities
 $string['edusyncheproctoring:view_report'] = 'Can view E-Proctoring Reports';
