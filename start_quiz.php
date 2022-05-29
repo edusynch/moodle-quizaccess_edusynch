1ccdcd9d6b968a50e1d724d8d13710322ef95071
Clenisson Calaça <clenisson.calaca@gmail.com>
2022-05-29 17:04:48 -0300

    FIX student not enabled

--- start_quiz.php
+++ start_quiz.php
@@ -29,20 +29,26 @@ $page      = required_param('page', PARAM_INT);
             $SESSION->edusynch_sessionid = $session_details['session_id'];        
             $SESSION->edusynch_token     = $session_details['token'];        
             $start_event = \quizaccess_edusynch\session::create_event_for($session_details['token'], $session_details['session_id'], 'START_SIMULATION');
+
+            $js = "$('button').trigger('click');";
         ?>    
         <form action="<?php echo "$CFG->wwwroot/$SESSION->edusynch_redirect" ?>" data-proctoring="form" method="GET">
             <input type="hidden" name="attempt" value="<?php echo $attemptid ?>">
             <input type="hidden" name="cmid" value="<?php echo $cmid ?>">
             <input type="hidden" name="page" value="<?php echo $page ?>">
             <button data-proctoring="start" data-id="<?php echo $session_details['session_id'] ?>" data-token="<?php echo $session_details['token'] ?>" type="submit" style="display: none"></button>
-        </form>
-
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>   
+        </form>      
+    <?php else: 
+        $SESSION->edusynch_started   = true;     
+        $js = " window.location.href = '$CFG->wwwroot/$SESSION->edusynch_redirect';";           
+    endif; ?>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>   
         <script type="text/javascript">
-            setTimeout(function() {
-                $('button').trigger('click');
-            }, 1000);    
-        </script>        
-    <?php endif; ?>
+        setTimeout(function() {
+            <?php echo $js; ?>
+        }, 1000);
+    </script>        
+    
     </body>
 </html>
