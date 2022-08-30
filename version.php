f98dbc963dbf704edf8226ced3e3564404134fd1
Clenisson Calaça <clenisson.calaca@gmail.com>
2022-08-30 08:55:19 -0300

    Updates version

--- version.php
+++ version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022072900;
+$plugin->version = 2022083000;
 $plugin->requires = 2018120309;
 $plugin->component = 'quizaccess_edusynch';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.4.1.2';
+$plugin->release = 'DBL_20220830';
 
 $plugin->dependencies = [
     'mod_quiz' => ANY_VERSION,
