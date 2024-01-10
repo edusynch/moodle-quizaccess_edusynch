010a88b740789a4705d91f30baea896c36a4ae95
Clenisson <clenisson.calaca@gmail.com>
2024-01-10 14:38:39 -0300

    FIX post actions

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024010800;
+$plugin->version = 2024011001;
 $plugin->requires = 2018120309;
 $plugin->component = 'quizaccess_edusynch';
 $plugin->maturity = MATURITY_STABLE;
