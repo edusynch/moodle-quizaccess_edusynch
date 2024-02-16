3e0e020dca70f5c3cb5e13e3aee391b9293fda8d
Clenisson <clenisson.calaca@gmail.com>
2024-02-16 12:09:38 -0300

    FIX version

--- version.php
+++ version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024011003;
+$plugin->version = 2024021601;
 $plugin->requires = 2018120309;
 $plugin->component = 'quizaccess_edusynch';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'KGP_20231205';
+$plugin->release = 'JKR_20240216';
 
 $plugin->dependencies = [
     'mod_quiz' => ANY_VERSION,
