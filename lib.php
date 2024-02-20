e23dab5bbcb3611618f28aed20917c13f4e48c42
Ruanito Santos <ruanitodiego@gmail.com>
2024-02-20 10:58:19 -0300

    INT-1452

--- lib.php
+++ lib.php
@@ -28,7 +28,7 @@ function quizaccess_edusynch_render_navbar_output() {
     $title = "EduSynch E-Proctoring";
     $url = new \moodle_url('/mod/quiz/accessrule/edusynch/index.php');
     if (!is_siteadmin()) {
-        $url = new \moodle_url('/mod/quiz/accessrule/edusynch/index.php?action=launch');
+        $url = new \moodle_url("/mod/quiz/accessrule/edusynch/index.php?action=launch&course_id=" . $_GET['id']);
     }
     $icon = new \pix_icon('i/hide', '');
     $node = \navigation_node::create($title, $url, \navigation_node::TYPE_CUSTOM, null, null, $icon);
@@ -42,9 +42,10 @@ function quizaccess_edusynch_render_navbar_output() {
     }
 
     if ($version[0] < '4') {
-        if (!is_siteadmin() && !in_array('Manager', $roles) && !str_contains($PAGE->url, 'course')) {
+        if (!is_siteadmin() && !in_array('Manager', $roles) && (!str_contains($PAGE->url, 'course'))) {
             return;
         }
+
         $PAGE->flatnav->add($node);
 
         return '';
