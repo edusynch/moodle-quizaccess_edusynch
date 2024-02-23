c1cc2abd19b942f37e3532e08fa34e456aca5e67
Ruanito Santos <ruanitodiego@gmail.com>
2024-02-23 12:42:55 -0300

    INT-1454

--- index.php
+++ index.php
@@ -47,6 +47,11 @@ $PAGE->set_heading(get_string('pluginname', 'quizaccess_edusynch'));
 $config      = new \quizaccess_edusynch\config();
 $config_key  = $config->get_key('oauth_token');
 
+function generateToken() {
+    $string = sha1(rand());
+    return substr($string, 0, 70);
+}
+
 if ($action != 'settings' && !$config_key) {
     echo $OUTPUT->header();
     include 'views/navbars.php';
@@ -63,12 +68,6 @@ if ($action != 'settings' && !$config_key) {
         require_capability('quizaccess/edusynch:edit_settings', $context);
     
         global $PAGE;
-    
-        if ($generate == 'token') {
-            $string = sha1(rand());
-            $token = substr($string, 0, 70);
-            $config->set_key('oauth_token', $token);
-        }
 
         if ($lti_url == 'save') {
             $lti_url = optional_param('url', '', PARAM_TEXT);
@@ -80,7 +79,8 @@ if ($action != 'settings' && !$config_key) {
 
         $token         = $config->get_key('oauth_token');
         $lti_url       = $config->get_key('lti_url');
-        $token_value   = $token ? $token->value : null;
+        $token_value   = $token ? $token->value : generateToken();
+        $saved_token   = $token ? substr_replace($token->value, '***********', 5) : '';
         $lti_url_value = $lti_url ? $lti_url->value : 'https://lti.edusynch.com';
     
     }  else if ($action == 'launch') {
@@ -112,7 +112,7 @@ if ($action != 'settings' && !$config_key) {
 
         $domain        = str_replace("/mod/quiz/accessrule/edusynch/index.php", "", $PAGE->url);
         $token         = $config->get_key('oauth_token');
-        $token_value   = $token ? $token->value : null;
+        $token_value   = $token ? $token->value : generateToken();
         $lti_url_value = $lti_url ? $lti_url->value : 'https://lti.edusynch.com';
     }
     
