a95d98642046ef33a2125e1ac30d4d2fbfd85fa9
Ruanito Santos <ruanitodiego@gmail.com>
2024-02-27 09:36:45 -0300

    INT-1454

---
+++ js/add-token.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  // Aguardando o front para finalizar essa parte
+  console.log($('#data-auth').attr('data-label'));
+  console.log($('#data-url').attr('data-label'));
+  console.log($('#data-draft-token').attr('data-label'));
+});
+
